58865a553f0fc464e07100e0b05e51ebbeb99945
Peter Vertesi <vpeter1119@gmail.com>
2020-05-13 18:53:55 +0200

    Add support for GeoJSON data fetched from API.

--- highrock/map.js
+++ highrock/map.js
@@ -1,30 +1,65 @@
-// Configuration
 
+/* Variables */
+
+var factorx = 0.0625;
+var factory = 0.0625;
+var mapheight = 4096;
+var mapwidth = 4096;
 var mapMinZoom = 1;
 var mapMaxZoom = 4;
-var bounds = [[0,0], [-2048,2048]];
 
-// Create map and layer
+/* Configure custom CRS */
+
+L.CRS.custom = L.extend({}, L.CRS.Simple, {
+  projection: L.Projection.LonLat,
+  transformation: new L.Transformation(factorx, 0, -factory, 0),
+
+  scale: function(zoom) {
+    return Math.pow(2, zoom);
+  },
+
+  zoom: function(scale) {
+    return Math.log(scale) / Math.LN2;
+  },
+
+  distance: function(latlng1, latlng2) {
+    var dx = latlng2.lng - latlng1.lng,
+		dy = latlng2.lat - latlng1.lat;
+
+    return Math.sqrt(dx * dx + dy * dy);
+  },
+  infinite: true
+});
+
+/* Create map and layer */
 
 var map = L.map('map', {
-    crs: L.CRS.Simple,
-	center: [0,0],
-	minZoom: 1,
-}).setView(new L.LatLng(-150,100), 1);
+	crs: L.CRS.custom
+}).setView([-2048, 2048], 1);
+
+// Set boundaries
+
+var sw = map.unproject([0, mapheight], 4);  // Level 4, because this is the level where meters-per-pixel is exactly 1
+var ne = map.unproject([mapwidth, 0], 4);
+var layerbounds = new L.LatLngBounds(sw, ne);
+
+// Get initial zoom level
 
 var currentZoomLevel = map.getZoom();
 
+// Add map tile layer
+
 L.tileLayer('tiles/{z}-{x}-{y}.jpg', {
-	bounds: bounds,
+	bounds: layerbounds,
 	tolerance: 0.8,
 	noWrap: true,
 	minZoom: mapMinZoom,
 	maxZoom: mapMaxZoom,
-	tms: false,
+	tms: true,
 	attribution: 'Peter Vertesi, 2020 | Created with <a href="https://www.wonderdraft.net/" target="_blank" rel="noopener noreferrer">Wonderdraft</a>'
 }).addTo(map);
 
-// Adjust zoom level
+// Show or hide markers based on zoom level
 
 function AdjustZoomLevel() {
 	currentZoomLevel = map.getZoom();
@@ -33,33 +68,102 @@ AdjustZoomLevel();
 map.on("zoomend", () => {
 	AdjustZoomLevel();
 	if (currentZoomLevel >= 3) {
-		map.addLayer(markersLayer);
+		map.addLayer(geoJsonLayerReference);
 	} else {
-		map.removeLayer(markersLayer);
+		map.removeLayer(geoJsonLayerReference);
 	}
 })
 
 // Markers
 
-var markersLayer = new L.LayerGroup(GetMarkers());
-console.warn(markersLayer);
+var dragMarker = L.marker([-496,192],{draggable:true}).addTo(map);
+// dragMarker is used to add new markers
+dragMarker.on("dragend", (e) => {
+	var coordinates = [e.target._latlng.lng,-e.target._latlng.lat];
+	dragMarker.bindPopup(`<h4>Current coordinates: ${coordinates}<h4><p><form onSubmit="return false"><label for="name">Name:</label><br><input type="text" id="name" name="name"><br><label for="url">Url:</label><br><input type="text" id="url" name="url"><br><label for="desc">Description:</label><br><input type="text" id="desc" name="desc"><input hidden type="text" id="coords" value="${coordinates}"><br><br><button type="submit" onClick="submitGeoJsonData(); dragMarker.closePopup();">Save</button></form></p>`).openPopup();
+})
 
-function GetMarkers() {
-	var markersArray = [];
-	markers.forEach(marker => {
-		var pos = [-(marker.position[1]/16),(marker.position[0]/16)];
-		console.warn(pos);
-		var options = marker.options;
-		//options.fillOpacity = 0;
-		var newMarker = L.marker(pos, options);
-		newMarker.bindPopup(marker.popup.content);
-		markersArray.push(newMarker);
+// GeoJSON
+var geoJsonLayerReference = L.geoJSON();
+var apiUrl = "http://localhost:3000/api/geojson";
+
+var queryUrl = `${apiUrl}?map=highrock`;
+httpGetAsync(queryUrl, (response) => {
+	var geoJsonData = JSON.parse(response).results;
+	var geoJsonLayer = L.geoJSON(false, {
+		onEachFeature: createGeoJsonPopup,
+		coordsToLatLng: convertGeoJsonCoordinates
 	});
-	return(markersArray);
+	if (currentZoomLevel >= 3) {
+		geoJsonLayer.addTo(map);
+	}
+	geoJsonLayer.addData(geoJsonData);
+	geoJsonLayerReference = geoJsonLayer;
+});
+
+/* Utility functions */
+
+// Make async HTTP GET request
+function httpGetAsync(url, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.response);
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous 
+    xmlHttp.send(null);
 }
 
-function FilterMarkers(zoomLevel) {
-	return markers.filter(marker => {
-		return (marker.zoomLevelMin <= zoomlevel && marker.zoomLevelMax >= zoomlevel);
-	});
+// Make async HTTP POST request
+function httpPostAsync(url, body, callback)
+{
+	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", url, true); // true for asynchronous
+	xmlHttp.setRequestHeader("Content-type","application/json");
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && (xmlHttp.status == 200 || xmlHttp.status == 201))
+            callback(xmlHttp.response);
+    }
+    xmlHttp.send(JSON.stringify(body));
+}
+
+// Bind popups based on GeoJSON properties data
+function createGeoJsonPopup(feature, layer)
+{
+	var popupData = `<h3><a href="${feature.properties.url}" target="_blank" rel="noopener noreferrer">${feature.properties.name}</a></h3>${feature.properties.desc}`;
+	layer.bindPopup(popupData);
+}
+
+// Convert geoJSON coordinates to correct LatLng values
+function convertGeoJsonCoordinates(coords)
+{
+	return new L.LatLng(-coords[1], coords[0], coords[2]);
+}
+
+// Create new pin
+function submitGeoJsonData() {
+	//Compile form data to correct format
+	var coordinates = document.getElementById("coords").value.split(",");
+	var data = {
+		geometry: {
+			type: "Point",
+			coordinates: [parseInt(coordinates[0]),parseInt(coordinates[1])]
+		},
+		properties: {
+			map: "highrock",
+			name: document.getElementById("name").value,
+			url: document.getElementById("url").value,
+			desc: `<p>${document.getElementById("desc").value}</p>`,
+			markerOptions: {}
+		}
+	};
+	//Make POST request to API
+	var token = prompt("Please enter the API token");
+	var postUrl = `${apiUrl}?token=${token}`;
+	httpPostAsync(postUrl, data, (response) => {
+		var message = JSON.parse(response).message;
+		window.alert(message);
+		window.location.reload(true);
+	})
 }
